7bfebcaac74ebcb0d1673a7aeffdcd911eda17f8
linda lacsivy <lindalacsivy@outlook.de>
2024-06-22 17:01:01 +0200

    swap() function done

--- command_functions.c
+++ command_functions.c
@@ -6,7 +6,7 @@
 /*   By: linda <linda@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 15:43:42 by linda             #+#    #+#             */
-/*   Updated: 2024/06/22 15:17:52 by linda            ###   ########.fr       */
+/*   Updated: 2024/06/22 16:59:00 by linda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@
 // we need to know if we have atleast 2 nodes
 void    swap(t_list_push_swap **stack, char stack_name) 
 {
-    // t_list_push_swap    *tmp;
-    // t_list_push_swap    tmp_node;
+    t_list_push_swap    *node1;
+    t_list_push_swap    *node2;
 
-    // tmp = *stack;
-    // tmp_node = ft_lstnew_push_swap();
-    // if (ft_lstsize_push_swap(*stack) >= 2)
-    // {
-        
-    // }
-    
-    // ft_printf("s%c\n", stack_name);
+    if (ft_lstsize_push_swap(*stack) >= 2)
+    {
+        node1 = extract_first_node(stack);
+        node2 = extract_first_node(stack);
+        ft_lstadd_front_push_swap(stack, node1);
+        ft_lstadd_front_push_swap(stack, node2);
+    }
+    ft_printf("s%c\n", stack_name);
 }
